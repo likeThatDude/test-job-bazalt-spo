5cb073ecad2d936b549615f0afdf6619b8e9f83b
Ivan Harbatsenka <109039577+likeThatDude@users.noreply.github.com>
2024-09-20 19:57:46 +0300

    Delete data.py

--- data.py
+++
@@ -1,6 +0,0 @@
-first = ['alt', 5, '.gita', 6, 'c', 6, 'b', 15, '.', 1]
-second = ['alt', 5, '.gita', 6, 'c', 6, 'b', 15]
-third = ['alt', 5, '.gita', 6, 'c', 6, 'b', 15]
-
-print(first == second)
-print(second == third)
